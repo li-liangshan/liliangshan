9cb24d72d15ecc555e0c326b98e570f0ef8dd09e
li-liangshan <liliangshanlls@gmail.com>
2018-11-30 11:18:32 +0800

    ss

--- build/bubble_sort_no.c
+++ build/bubble_sort_no.c
@@ -2,20 +2,13 @@
 
 void bubble_sort(int arr[], int length) {
 	int temp;
-	int ex = 0;
 	for (int i = length; i > 0; i--) {  //外层循环移动游标
-		ex = 0;
     for (int j = 0; j < i && (j+1) < i; j++) {    //内层循环遍历游标及之后(或之前)的元素
       if(arr[j] > arr[j+1]) {
           int temp = arr[j];
           arr[j] = arr[j+1];
           arr[j+1] = temp;
-					ex = 1;
        }
-     }
-
-     if (!ex) {
-      return;
      }
    }
 }
@@ -26,8 +19,7 @@ int main() {
     int len = (int) sizeof(arr) / sizeof(*arr);
     bubble_sort(arr, len);
     int i;
-    for (i = 0; i < len; i++)
-    {
+    for (i = 0; i < len; i++) {
       printf("%d ", arr[i]);
     }
     printf("\n");
